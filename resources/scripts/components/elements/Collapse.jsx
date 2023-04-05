f66966d26814931e2ed6a01da9f024bda393ac3f
Ferks-FK <fernandokaiquecnp2014@gmail.com>
2023-04-05 16:23:03 +0000

    Add translation cache and fix others stuffs

--- resources/scripts/components/elements/Collapse.jsx
+++ resources/scripts/components/elements/Collapse.jsx
@@ -1,13 +1,13 @@
 import { useRef } from "react"
 
 function Collapse({ children, visible }) {
-  const contentRef = useRef();
+  const contentRef = useRef(0);
 
   return (
     <div
       className="h-0 overflow-hidden transition-all duration-300 ease"
       ref={contentRef}
-      style={visible ? { height: contentRef.current.scrollHeight + "px" } : { height: "0px" }}
+      style={visible ? { height: contentRef.current.scrollHeight == undefined ? '250px' : contentRef.current.scrollHeight } : { height: "0px" }}
     >
       {children}
     </div>
--- resources/scripts/i18n.js
+++ resources/scripts/i18n.js
@@ -1,7 +1,10 @@
-import i18next from 'i18next'
+import i18next from 'i18next';
 import I18NextHttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector'
-import { initReactI18next } from 'react-i18next'
+import HttpBackend from "i18next-http-backend";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import ChainedBackend from 'i18next-chained-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend';
+import { initReactI18next } from 'react-i18next';
 
 export const SupportedLanguages = [
   {
@@ -23,6 +26,7 @@ const supportedLngs = SupportedLanguages.map(({ code }) => (code)) // Only suppo
 i18next
   .use(LanguageDetector)
   .use(I18NextHttpBackend)
+  .use(ChainedBackend)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
@@ -34,7 +38,18 @@ i18next
     interpolation: {
       escapeValue: false
     },
-    allowMultiLoading: true
+    allowMultiLoading: false,
+    backend: {
+      backends: [
+        LocalStorageBackend,
+        HttpBackend
+      ],
+      backendOptions: [{
+        expirationTime: 3 * 24 * 60 * 60 * 1000 // 3 days
+      }, {
+        loadPath: '/locales/{{lng}}/{{ns}}.json'
+      }]
+    }
   })
 
 export default i18next;
