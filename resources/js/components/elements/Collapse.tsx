719a5e702cf5ce6704caea8e09cdc4381ccfc2f6
Ferks-FK <fernandokaiquecnp2014@gmail.com>
2023-07-08 13:39:06 +0000

    style: :lipstick: Fix collapse component height

--- resources/js/components/elements/Collapse.tsx
+++ resources/js/components/elements/Collapse.tsx
@@ -1,17 +1,26 @@
 import classNames from "classnames";
 import { HTMLAttributes } from "react";
+import { useRef, useState, useEffect } from "react";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   visible: boolean
 }
 
 const Collapse = ({ children, visible }: Props) => {
+  const [containerHeight, setContainerHeight] = useState(0);
+  const containerRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setContainerHeight(containerRef.current.scrollHeight)
+    }
+  }, [])
+
   return (
     <div
-      className={classNames('h-0 overflow-hidden transition-all duration-300 ease',
-      {
-        ['h-60']: visible === true
-      })}
+      ref={containerRef}
+      style={{ height: visible ? containerHeight : 0 }}
+      className={classNames('max-h-60 overflow-hidden transition-all duration-300 ease')}
     >
       {children}
     </div>
